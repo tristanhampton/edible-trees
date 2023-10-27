92919557b59f276498bc9fa9512af3e5f5d00d25
Tristan Hampton <tristanhampton@icloud.com>
2023-10-27 16:36:11 -0600

    Map centers on user

--- src/components/api/UserLocationMarker.js
+++ src/components/api/UserLocationMarker.js
@@ -1,18 +1,21 @@
 import { useState } from "react"
-import { Marker, useMapEvents } from "react-leaflet"
+import { Marker, useMap, useMapEvent } from "react-leaflet"
 import { iconPerson } from "./Icon";
 
 export default function UserLocationMarker() {
   const [position, setPosition] = useState(null)
-  const map = useMapEvents({
-    click() {
-      map.locate()
-    },
+  const map = useMap();
+
+  // locate user
+  map.locate();
+
+  // when user is found, center on them
+  useMapEvent({
     locationfound(e) {
       setPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
+      map.flyTo(e.latlng, 14)
     },
-  });
+  })
 
   return position === null ? null : (
     <Marker position={position} icon={iconPerson} onClick={e => e.preventDefault()}/>
