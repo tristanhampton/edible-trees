32af8867a3fc997e8e7512fa427922c4261d271a
Tristan Hampton <tristanhampton@icloud.com>
2023-11-01 16:21:21 -0600

    Added marker clusters

--- src/components/api/FruitRequestLeaflet.js
+++ src/components/api/FruitRequestLeaflet.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import UserLocationMarker from "./UserLocationMarker";
-
+import MarkerClusterGroup from '@changey/react-leaflet-markercluster'
 
 const FruitRequestLeaflet = () => {
   const [data, setData] = useState(null);
@@ -10,7 +10,7 @@ const FruitRequestLeaflet = () => {
   const center = { lat: 53.605598645371686, lng: -113.54153970136359 };
 
   useEffect(() => {
-    fetch('https://data.edmonton.ca/resource/eecg-fc54.json?bears_edible_fruit=true&$limit=200')
+    fetch('https://data.edmonton.ca/resource/eecg-fc54.json?bears_edible_fruit=true')
       .then(response => response.json())
       .then(json => {
         setData(json);
@@ -28,27 +28,28 @@ const FruitRequestLeaflet = () => {
   }
 
   return (
-    <>
       <MapContainer center={center} zoom={12}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
 
-        {data.map((tree, index) => (
-          <Marker
-            key={index}
-            position={{ lat: parseFloat(tree.latitude), lng: parseFloat(tree.longitude) }}
-          >
-            <Popup>
-              <h3>{tree.type_of_edible_fruit}</h3>
-            </Popup>
-          </Marker>
-        ))}
+        <MarkerClusterGroup>
+          {data.map((tree, index) => (
+            <Marker
+              key={index}
+              position={{ lat: parseFloat(tree.latitude), lng: parseFloat(tree.longitude) }}
+            >
+              <Popup>
+                <h3>{tree.type_of_edible_fruit}</h3>
+              </Popup>
+            </Marker>
+          ))}
+
+        </MarkerClusterGroup>
 
         <UserLocationMarker />
       </MapContainer>
-    </>
   );
 }
 
